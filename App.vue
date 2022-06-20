1ebb0664c6f03e58e8ef184c598d93ec0bad60e1
Jaiden Groenland <94353701+JaidenGroenland@users.noreply.github.com>
2022-06-20 11:42:12 +0200

    Add files via upload
    alles toegevoegd want ik had slechte commits

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <VacationPicker />
+</template>
+
+<script>
+import VacationPicker from "@/components/VacationPicker";
+export default {
+  name: 'App',
+  components: {
+    VacationPicker
+  }
+}
+</script>
+
+<style>
+</style>
